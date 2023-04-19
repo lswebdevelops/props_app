d02e0fc5deb787a110e3f438419af30cebff1ec3
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-18 21:14:51 -0300

    adding a functional component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import './App.css';
 import React from "react";
+import MyComponent from './MyComponent';
 
-
-class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      count: 0,
-    };
-    
-    this.countUp = this.countUp.bind(this);
-   
-  }
-
-  countUp(){
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
-
-  
-
-  
-  render(){
-    return(
-      <div>
-        <button onClick={this.countUp}>
-          Click Me!
-        </button>
-        <p>{this.state.count}</p>
-          
-      </div>
-    );
-  }
+function App(){
+  return(
+    <div>
+      <MyComponent 
+      title="Hello World!" />
+    </div>
+  )
 }
 
 export default App;
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,31 +1,8 @@
+import React from "react";
 
-import React, { Component } from 'react';
 
-class MyComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
-// using  Destructuring 
-  render() {
-    const { title, onButtonClicked } = this.props;
-
-    return (
-      <div>
-        <h1>{title}</h1>
-        <button onClick={onButtonClicked}>Click Me!</button>
-      </div>
-    );
-  }
+function MyComponent(props){
+  return <div>{props.title}</div>;
 }
-// it was like : 
-// render() {
-//     return (
-//       <div>
-//         <h1>{this.props.title}</h1>
-//         <button onClick={this.props.onButtonClicked}>Click Me!</button>
-//       </div>
-//     );
-//   }
-// }
 
 export default MyComponent;
