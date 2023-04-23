8ca0b69d2616cbb0c5b945332e49f0ddde2f25ba
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-23 19:35:46 -0300

    adding styling condicion with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import React from "react";
-import MyComponent from './MyComponent';
+import Jokes from './Components/Jokes';
 
 function App(){
   return(
     <div>
-      <MyComponent />
-          </div>
+      <Jokes
+      setup="I got my daughter a fridge for her birthday" 
+      punchline="I can't wait to see her face light up when she opens it."
+      />
+      <Jokes
+      setup="How did the hacker escape the police?" 
+      punchline="He ransomwared himself."
+      />
+        <Jokes
+      setup="Why don't pirates travel on mountain roads?" 
+      punchline="Because they prefer to take the high seas."
+      />
+      
+      <Jokes
+      setup="Why do bees stay in the hive in the winter?" 
+      punchline="It's too cold to go outside and pollen-tate."
+      />
+
+        <Jokes
+      setup="What's the best hing about Switzerland?" 
+      punchline="I don't know, but the flag is a big plus."
+      />
+    </div>
   )
 }
 
