8dc1381893935979bfb399648cfb83280a676af1
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-04 21:58:00 -0300

    using props.id to toogle the background color style

--- src/App.js
+++ src/App.js
@@ -6,18 +6,37 @@ import boxes from './Data/dataBoxes';
 
 function App(){
   const [squares, setSquares] = React.useState(boxes);
+ 
+  function toggle(id){
+    setSquares(prevSquares =>{
+      const newSquares = []
+      for(let i = 0; i <prevSquares.length; i++){
+        const currentSquare = prevSquares[i]
+        if(currentSquare.id === id){
+          const updatedSquare = {
+            ...currentSquare, 
+            on: !currentSquare.on
+          }
+          newSquares.push(updatedSquare)
+        }else{
+          newSquares.push(currentSquare)
+        }
+      }
+      
+      return newSquares
+    })
+ 
+  }
   const squaresElements = squares.map(square =>(
     <Box
     key={square.id}
     id={square.id}
     on={square.on}
     emoji={square.emoji}
-    handleClick={toggle}
+    handleToggle={toggle}
     />
   ))
-  function toggle(id){
-    console.log(id);
-    }
+
   return(
     <div >
 
--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -3,17 +3,16 @@ import React from 'react';
 import '../Styles/Box.css';
 
 function Box(props){
-  const [on, setOn]= React.useState(props.on);
-
+ 
   const styles={
-    backgroundColor: on? "#555555": "transparent"
+    backgroundColor: props.on? "#555555": "transparent"
   }
 
  return(
     <div 
       style={styles}
       className="box"
-      onClick={() => props.handleClick(props.id)}
+      onClick={() => props.handleToggle(props.id)}
 
       >{props.emoji}</div>
   )
