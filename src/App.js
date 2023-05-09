a25da3facffa96de0f5f69f2354437799d2d4e7f
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-08 21:46:48 -0300

    adding icon  to messages

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
 import "./App.css";
 import React from "react";
-
+import { TbDeviceMobileMessage } from "react-icons/tb";
 
 function App() {
- const [messages, setMessages] = React.useState(["a"])
-
-  //  if(messages.length === 1){
-    
-  //   return(<div>
-  //   <h1>{`Your have ${messages.length} unread message!`}</h1> 
-  //   </div>
-  //   )
-  //  }else if(messages.length > 1){
-  //   return (
-  //     <div>
-  //       <h1>{`Your have ${messages.length} unread messages!`}</h1> 
-
-  //     </div>
-  //   )
-
-  //  }else{
-  //   return null;
-  //  }
-  //  now using ternary: 
+ const [messages, setMessages] = React.useState(["this is one message"])
 
 
 return(
- <div>
-   {
-    messages.length === 0?
-    null:
-    // <h1>You have {messages.length} unread message{messages.length > 1 && "s"}!</h1>
-    //  a better writen/ understandable code: 
-    <h1>You have {messages.length} unread 
-    {messages.length > 1 ? " messages" : " message"}!</h1>
-}
-
- </div>
-
+  <div>
+    {
+      messages.length === 0 ? 
+      null:
+      <h1> <TbDeviceMobileMessage/> You have {messages.length} {messages.length === 1? "message": "messages"}!</h1>
+    }
+  </div>
 )
 }
 
