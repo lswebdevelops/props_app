c6047dc1ad3ac6d7630b7de2acf89727efb756de
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-11 21:27:54 -0300

    adding a cleanup function to remove eventListener

--- src/Components/WindowTracker.jsx
+++ src/Components/WindowTracker.jsx
@@ -4,9 +4,15 @@ function WindowTracker() {
   const [windowWidth, setwindowWidth] = React.useState(window.innerWidth);
 
   React.useEffect(() => {
-    window.addEventListener("resize", function () {
+    function watchWidth() {
+        // console.log("Setting up...");
       setwindowWidth(window.innerWidth);
-    });
+    }
+    window.addEventListener("resize", watchWidth);
+    return function () {
+    //   console.log("cleaning up!!");
+      window.removeEventListener("resize", watchWidth);
+    };
   }, []);
   return <h1>Window width: {windowWidth}</h1>;
 }
