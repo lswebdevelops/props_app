ba97d9e0404571500e6e09412eaca042e7c463d4
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-11 21:18:55 -0300

    updating the width printed on screen by resizing

--- src/Components/WindowTracker.jsx
+++ src/Components/WindowTracker.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-function WindowTracker(){
-    return(
-       <h1>Window width: {window.innerWidth}</h1>
-    )
+function WindowTracker() {
+  const [windowWidth, setwindowWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", function () {
+      setwindowWidth(window.innerWidth);
+    });
+  }, []);
+  return <h1>Window width: {windowWidth}</h1>;
 }
 
 export default WindowTracker;
