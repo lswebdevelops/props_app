9566d43b898ec58a336aa8b322f1e894d52d2213
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-16 16:24:03 -0300

    using destructuring and class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import "./App.css";
-import React from "react";
-import ThemeToggle from "./Components/DarkLightMode";
 
-function App() {
-  
-  return (
-    <div>
-    <ThemeToggle />
-    </div>
-  );
+import React, { Component } from 'react';
+import MyComponent from './Components/MyComponent';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onClickBtn = this.onClickBtn.bind(this);
+  }
+
+  onClickBtn() {
+    console.log('Button has been clicked!');
+  }
+
+  render() {
+    return (
+      <div>
+        <MyComponent title="React" onButtonClicked={this.onClickBtn} />
+      </div>
+    );
+  }
 }
 
 export default App;
