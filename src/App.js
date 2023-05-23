f1f321c4e7c4b42e3ec24f1f5cf1b9309ea19505
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-23 20:56:10 -0300

    using hooks, useState

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import './App.css';
-import Counter from './Components/Counter';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mount: true
-    };
-    this.mountCounter = () => this.setState({ mount: true });
-    this.unMountCounter = () => this.setState({ mount: false });
-  }
+const App = () => {
+  const [count , setCount ] = React.useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  const decrementCount = () => {
+    setCount(count -1);
+  };
+  return(
+    <div>
+      <div>{count}</div>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.mountCounter} disabled={this.state.mount}>
-          Mount
-        </button>
-        <button onClick={this.unMountCounter} disabled={!this.state.mount}>
-          Unmount
-        </button>
-
-        {this.state.mount ? <Counter /> : null}
-      </div>
-    );
-  }
+    </div>
+  )
 }
-
 export default App;
