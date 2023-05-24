a8f0b2cb243216895a09f3f6632d03cf7219e7ff
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-23 21:31:49 -0300

    changing color of div

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from "react";
+
 
 const App = () => {
-  const [count , setCount ] = React.useState(0);
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-  const decrementCount = () => {
-    setCount(count -1);
-  };
+  const [ color, setColor ] = useState('black');
+  useEffect(() => {
+    const chageColorOnClick = () => {
+      if (color === "black") {
+        setColor("red");
+      }else{
+        setColor("black");
+      }
+    };
+
+    document.addEventListener("click", chageColorOnClick);
+    return() => {
+      document.removeEventListener( "click", chageColorOnClick);
+    };
+  }, [color]);
   return(
     <div>
-      <div>{count}</div>
-      <button onClick={incrementCount}>Increment</button>
-      <button onClick={decrementCount}>Decrement</button>
+      <div
+      id="myDiv"
+      style={{
+        color: "white",
+        width: "100px",
+        height: "100px",
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        backgroundColor: color,
 
+      }}
+      >
+        This div can change color. If you click on it! 
+      </div>
     </div>
   )
 }
+
 export default App;
